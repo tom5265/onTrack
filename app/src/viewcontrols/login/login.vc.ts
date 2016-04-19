724f91851e038f8eee9ca580f9cf9803593086cf
Tom Moore <tommoore@Toms-MacBook-Pro.local>
2016-04-19 16:46:33 -0500

    login functoin added

--- app/src/viewcontrols/login/login.vc.ts
+++ app/src/viewcontrols/login/login.vc.ts
@@ -5,6 +5,29 @@ export default class LoginViewControl extends BaseViewControl {
     templateString: string = require('./login.vc.html');
 
     context: any = {};
+
+    logInUser() {
+        let email: string = jQuery('#emailInput').val();
+        let password: string = jQuery('#passwordInput').val();
+        this.authenticate(email, password);
+    }
+
+    authenticate(email:string , password:string) {
+        ref.authWithPassword({
+            email: email,
+            password: password
+        }, function (error, authData) {
+            if (error) {
+                console.log("Login Failed!", error);
+            } else {
+                console.log("Authenticated successfully with payload:", authData);
+            }
+        });
+
+    }
+
 }
+
+var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
 
 register.viewControl('login-vc', LoginViewControl);
