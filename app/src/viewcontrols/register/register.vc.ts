513fc137fe00111d54a76ea02146f98675557bb5
Tom Moore <tommoore@Toms-MacBook-Pro.local>
2016-04-19 15:20:59 -0500

    finishing setup

--- app/src/viewcontrols/register/register.vc.ts
+++ app/src/viewcontrols/register/register.vc.ts
@@ -1,10 +1,31 @@
 import {register} from 'platypus';
 import BaseViewControl from '../base/base.vc';
+import * as jQuery from 'jquery';
+import firebaserepository from '../../repositories/firebase/firebase.repo';
 
 export default class RegisterViewControl extends BaseViewControl {
     templateString: string = require('./register.vc.html');
 
     context: any = {};
+
+    constructor(private firebaserepo: firebaserepository) {
+        super();
+    };
+
+
+
+
+    createUser(email: string, password: string) {
+        this.firebaserepo.createUser(email, password).then((success: any) => {
+            console.log('success');
+        }, (err: any) => {
+            console.log('something went wrong!');
+            console.log(err);
+        });
+
+
+    }
 }
+
 
 register.viewControl('register-vc', RegisterViewControl);
