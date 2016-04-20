d24b4602e11b6115c7e4fd0971cc98a6471746f2
Chase Brasfield <chasebrasfield@gmail.com>
2016-04-20 14:14:14 -0500

    fixed some stuff

--- app/src/viewcontrols/newtask/newtask.vc.ts
+++ app/src/viewcontrols/newtask/newtask.vc.ts
@@ -6,29 +6,38 @@ import * as jQuery from 'jquery';
 
 export default class NewtaskViewControl extends BaseViewControl {
     templateString: string = require('./newtask.vc.html');
-    constructor(private firebaserepo: FirebaseRepository){
+    constructor(private firebaserepo: FirebaseRepository) {
         super();
     }
 
+
     context: any = {
+        UID: '',
         SingleTask: {
             taskName: "",
             taskObjectives: {}
         }
     };
-    
-    postTask(){
+
+    postTask() {
         console.log(this.context.SingleTask.taskName);
         this.firebaserepo.postUserTask(this.context.SingleTask);
-        this.navigator.navigate(HomeViewControl);
+
+        this.navigator.navigate(HomeViewControl, {
+            parameters: {
+                id: this.context.UID
+            }
+        })
     }
-    
-    navigatedTo(parameters: { id: string; }){
+
+
+    navigatedTo(parameters: { id: string; }) {
         console.log(parameters.id);
         this.context.userId = parameters.id;
+        this.context.UID = parameters.id;
     }
-    
-    addCheckpoint(){
+
+    addCheckpoint() {
         jQuery('.checkpoint-container').append('<input type="text" placeholder="Checkpoint">');
     }
 }
