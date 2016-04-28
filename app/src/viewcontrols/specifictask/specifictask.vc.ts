fa4667edcc01d210544a3201a9b8d921112598d8
Rehubbard <rehubbard@crimson.ua.edu>
2016-04-28 17:47:09 -0500

    Fixed when you nav to specific task with every task complete so the button shows 'complete task'.

--- app/src/viewcontrols/specifictask/specifictask.vc.ts
+++ app/src/viewcontrols/specifictask/specifictask.vc.ts
@@ -26,22 +26,20 @@ export default class SpecifictaskViewControl extends BaseViewControl {
     constructor(private firebaserepo: FirebaseRepository) {
         super();
     }
-    
 
 
-    navigatedTo(parameters: { key: string, id:string}) {
-        console.log('inside navto');
-        console.log(parameters.key);
-        let tempArray: any = null;
+
+    navigatedTo(parameters: { key: string, id: string }) {
         let key = parameters.key;
         this.publickey = key;
         let myDataRefPosts = new Firebase('https://popping-inferno-1046.firebaseIO.com/users/' + this.firebaserepo.userID + '/' + key);
         myDataRefPosts.on("value", (snapshot: any, prevChildKey: any) => {
             let data = snapshot.val();
-            if(data.task.completionDate === ''){
+            //hides or shows date container if date is present
+            if (data.task.completionDate === '') {
                 console.log('no date set');
                 jQuery('.date-container').hide();
-            }else{
+            } else {
                 console.log('date set');
                 jQuery('.date-container').show();
             }
@@ -55,13 +53,16 @@ export default class SpecifictaskViewControl extends BaseViewControl {
             this.context.checkpoints = task.taskObjectives.length;
             let numberCompleted = 0;
             task.taskObjectives.forEach((obj: any) => {
+                console.log(obj.isCompleted);
                 if (obj.isCompleted) {
                     numberCompleted++;
                 }
             });
             this.context.completedCheckpoints = numberCompleted;
-            if (numberCompleted > 0) {
-                // console.log('o');
+            //setTimeout 
+            setTimeout(function(){
+                if (numberCompleted > 0) {
+                console.log('o');
                 if (task.taskObjectives.length === numberCompleted) {
                     let bg = document.getElementById('color-bg');
                     bg.style.backgroundColor = "rgba(201,278,114,0.1)";
@@ -70,82 +71,94 @@ export default class SpecifictaskViewControl extends BaseViewControl {
                     button.addClass('animated');
                     button.addClass('tada');
                     button.addClass('complete-button');
-                    button.css({'backgroundColor' : '#68C3A3','borderColor' : '#68C3A3'})
+                    button.css({ 'backgroundColor': '#68C3A3', 'borderColor': '#68C3A3' })
                 } else {
                     let bg = document.getElementById('color-bg')
                     bg.style.backgroundColor = 'rgba(250,255,255,0.2)';
                     let button = jQuery('#deleteButton');
                     button.removeClass('animated tada complete-button')
                     button.text('Delete Task');
-                    button.css({'backgroundColor' : '#ef716f','borderColor' : '#ef716f'})
+                    button.css({ 'backgroundColor': '#ef716f', 'borderColor': '#ef716f' })
                 }
+            } else {
+                let bg = document.getElementById('color-bg')
+                bg.style.backgroundColor = 'rgba(250,255,255,0.2)';
+                let button = jQuery('#deleteButton');
+                button.removeClass('animated tada complete-button')
+                button.text('Delete Task');
+                button.css({ 'backgroundColor': '#ef716f', 'borderColor': '#ef716f' })
             }
-                console.log(this.context.specificTask);  
-                this.compareDates(this.context.specificTask.completionDate);
-        });   
+            }, 100)
+            console.log(this.context.specificTask);  
+            this.compareDates(this.context.specificTask.completionDate);
+        });
     };
-    
-    loaded(){
+
+    loaded() {
         this.compareDates(this.context.specificTask.completionDate);
     }
-    
-    deleteThisPost() {
 
+    deleteThisPost(){
         if (this.context.checkpoints != this.context.completedCheckpoints) {
             this.context.modal2 = true;
         } else {
-            if(jQuery(".off-track-bubble").css('display') === 'block'){
+            if (jQuery(".off-track-bubble").css('display') === 'block') {
                 this.context.modal3 = true;
             }
-            if(jQuery(".on-track-bubble").css('display') === 'block'){
+            if (jQuery(".on-track-bubble").css('display') === 'block') {
                 this.context.modal = true;
+            }
+            if(jQuery('.on-track-bubble').css('display') === 'none' && jQuery('.off-track-bubble').css('display') === 'none'){
+               this.context.modal3 = true;
             }
         }
     }
-    
-    hideModal(num:number){
-        if(num === 1){
+
+    hideModal(num: number) {
+        if (num === 1) {
             this.context.modal = false;
         }
-        if(num === 2){
+        if (num === 2) {
             this.context.modal2 = false;
         }
-        
-        if(num === 3){
+
+        if (num === 3) {
             this.context.modal3 = false;
         }
     }
-    
-    continueModal(){
+
+    continueModal() {
         this.firebaserepo.deleteThisPost(this.publickey, this.firebaserepo.userID);
         this.navigator.navigate(HomeViewControl);
     }
 
     someFunc() {
+
         let numberOfTrues = document.getElementsByClassName('true');
+        console.log(numberOfTrues);
         this.context.completedCheckpoints = numberOfTrues.length;
         let numberOfTasks = document.getElementsByClassName('task');
         this.context.checkpoints = numberOfTasks.length;
-        if (numberOfTrues.length > 0) {
+        if (numberOfTrues.length >= 0) {
             if (numberOfTrues == numberOfTasks) {
-                console.log('congrats');
+                // console.log('congrats');
             }
         }
         this.firebaserepo.updateUserTask(this.context.specificTask, this.publickey);
     }
-    
-    compareDates(setDate:any) {
-        console.log('comparing...');
+
+    compareDates(setDate: any) {
+        // console.log('comparing...');
         let now = new Date();
         let set = new Date(setDate);
-        if(this.context.specificTask.completionDate !== ''){ //if date is present
+        if (this.context.specificTask.completionDate !== '') { //if date is present
             if (now < set) { //if on track
                 jQuery('#on-track').show()
             } else { //if too late
                 jQuery('#off-track').show()
             }
-        }else{
-            console.log('no date');
+        } else {
+            // console.log('no date');
         }
     }
 };
